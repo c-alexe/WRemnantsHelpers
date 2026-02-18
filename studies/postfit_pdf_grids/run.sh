1f0e68693fadc60dadd15e6906657bc1c62a83a0
c-alexe <alexe.cristina99@gmail.com>
2026-02-18 23:27:15 +0100

    added script to run pdf plots

---
+++ studies/postfit_pdf_grids/run.sh
@@ -0,0 +1,128 @@
+
+#!/usr/bin/env bash
+# Make the PDF grids and plot results
+
+usage() {
+    echo "Usage: run.sh <infile> -o <output_dir>" 
+    echo "-f <short name of fitted PDF>"
+    echo "-p <short name of pd PDF>"
+    echo "--plotStepOnly <only plot the PDFs, skip previous steps>"
+    #TODO make a dryrun option
+    echo "-h, --help <show this help message>"
+    exit 1
+}
+
+infile="/scratch/calexe/PostfitPdfStudies/w_z_gen_dists_maxFiles_m1_nnpdf40_pdMSHT20_masswindow.hdf5"
+output_dir="/scratch/calexe/PostfitPdfStudies/GenFits/"
+fittedPDF="nnpdf40"
+pdPDF="msht20"
+plotStepOnly=false
+
+if [ -z "$1" ]; then
+    usage
+fi
+
+input_file=$1
+shift
+
+PARSED=$(getopt -o o:f:p:h --long output:,fittedPDF:,pdPDF:,plotStepOnly,help -- "$@")
+if [[ $? -ne 0 ]]; then
+    echo "Failed to parse arguments." >&2
+    exit 1
+fi
+eval set -- "$PARSED"
+
+while true; do
+    case "$1" in
+        -o|--output)
+            output_dir="$2"
+            shift 2
+            ;;
+        -f|--fittedPDF)
+            fittedPDF="$2"
+            shift 2
+            ;;
+        -p|--pdPDF)
+            pdPDF="$2"
+            shift 2
+            ;;
+        --plotStepOnly)
+            plotStepOnly=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+echo
+echo "setupRabbit"
+
+asterix_args=(
+    --excludeNuisances '.*'
+    --keepNuisances 'pdf.*'
+)    
+
+postfix_arg="${fittedPDF,,}_pd${pdPDF^^}"
+
+setupRabbit_command="python3 ${WREM_BASE}/scripts/rabbit/setupRabbit.py -i $input_file --fitvar ptVgen-absYVgen -o $output_dir --filter Zmumu ${asterix_args[@]} --pseudoData nominal_gen_pdf${pdPDF^^} --postfix $postfix_arg -n nominal_gen --resumUnc none --npUnc none --asUncFromUncorr --noTheoryCorrsViaHel --scalePdf 1. --pseudoDataIdx 0 --pseudoDataAxes pdfVar"
+echo "$setupRabbit_command"
+
+if ! $plotStepOnly; then 
+    setupRabbit_output=$($setupRabbit_command 2>&1 | tee /dev/tty)
+else
+    setupRabbit_output=" "
+fi
+
+echo
+echo "rabbit_fit"
+
+carrot=$(echo "$setupRabbit_output" | grep -oP '(?<=Write output file ).*')
+carrot=$(echo "$carrot" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g') # sanitize the output
+echo "Rabbit file: $carrot"
+output=$(dirname "$carrot")
+echo "Output: $output"
+
+rabbit_fit_command="rabbit_fit.py $carrot -t 0 --doImpacts --unblind --saveHists --saveHistsPerProcess --computeHistErrors -m BaseMapping -m Project ch0 absYVgen -m Project ch0 ptVgen --pseudoData nominal_gen_pdf${pdPDF^^}_pdfVar -o $output"
+echo "$rabbit_fit_command"
+
+if ! $plotStepOnly; then 
+    rabbit_fit_output=$($rabbit_fit_command 2>&1 | tee /dev/tty)
+else
+    rabbit_fit_output=" "
+fi
+
+fit_result=$(echo "$rabbit_fit_output" | grep -oP '(?<=Results written in file ).*')
+#fit_result=$(echo "$fit_result" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g') # sanitize the output
+echo "$fit_result"
+
+echo
+echo "make_postfit_pdf_grids"
+
+if ! $plotStepOnly; then python3 ${WREM_BASE}/scripts/utilities/make_postfit_pdf_grids.py --fitresult $fit_result -o $output_dir --pseudoData nominal_gen_pdf${pdPDF^^}_pdfVar -l $postfix_arg
+fi
+
+# if the output plots dir doesn't exist, create it
+output_plot_dir="${output_dir}/GenFit${fittedPDF^^}pd${pdPDF^^}/"
+if [ ! -d "$output_plot_dir" ]; then
+    mkdir -p "$output_plot_dir"
+fi
+echo "Output plots directory: $output_plot_dir" 
+
+
+compound_label_value="'${fittedPDF^^} (gen fit ${pdPDF^^})'"
+
+python3 ${WREM_BASE}/scripts/plotting/plot_pdfs.py --lhapdf-path $output_dir -s NNPDF40_nnlo_hessian_pdfas MSHT20nnlo_as118 NNPDF40_nnlo_hessian_pdfas_nnpdf40_pdMSHT20_unscaled -o $output_plot_dir -l ${fittedPDF^^} ${pdPDF^^} $compound_label_value -f dv uv 1 2 3 --colors r g b
+#echo "$plot_command"
+#plot_output=$($plot_command 2>&1 | tee /dev/tty)
+
+
