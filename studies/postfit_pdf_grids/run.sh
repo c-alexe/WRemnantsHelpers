ba7bcdbac0e751346dc595b7ac046c8c5b8c59bf
c-alexe <alexe.cristina99@gmail.com>
2026-02-20 13:23:59 +0100

    improved plot step

--- studies/postfit_pdf_grids/run.sh
+++ studies/postfit_pdf_grids/run.sh
@@ -6,8 +6,6 @@ usage() {
     echo "Usage: run.sh <infile> -o <output_dir>" 
     echo "-f <short name of fitted PDF>"
     echo "-p <short name of pd PDF>"
-    echo "--plotStepOnly <only plot the PDFs, skip previous steps>"
-    #TODO make a dryrun option
     echo "-h, --help <show this help message>"
     exit 1
 }
@@ -16,7 +14,6 @@ infile="/scratch/calexe/PostfitPdfStudies/w_z_gen_dists_maxFiles_m1_nnpdf40_pdMS
 output_dir="/scratch/calexe/PostfitPdfStudies/GenFits/"
 fittedPDF="nnpdf40"
 pdPDF="msht20"
-plotStepOnly=false
 
 if [ -z "$1" ]; then
     usage
@@ -25,7 +22,7 @@ fi
 input_file=$1
 shift
 
-PARSED=$(getopt -o o:f:p:h --long output:,fittedPDF:,pdPDF:,plotStepOnly,help -- "$@")
+PARSED=$(getopt -o o:f:p:h --long output:,fittedPDF:,pdPDF:,help -- "$@")
 if [[ $? -ne 0 ]]; then
     echo "Failed to parse arguments." >&2
     exit 1
@@ -46,10 +43,6 @@ while true; do
             pdPDF="$2"
             shift 2
             ;;
-        --plotStepOnly)
-            plotStepOnly=true
-            shift
-            ;;
         -h|--help)
             usage
             ;;
@@ -65,7 +58,7 @@ while true; do
 done
 
 echo
-echo "setupRabbit"
+echo "1) setupRabbit"
 
 asterix_args=(
     --excludeNuisances '.*'
@@ -75,54 +68,73 @@ asterix_args=(
 postfix_arg="${fittedPDF,,}_pd${pdPDF^^}"
 
 setupRabbit_command="python3 ${WREM_BASE}/scripts/rabbit/setupRabbit.py -i $input_file --fitvar ptVgen-absYVgen -o $output_dir --filter Zmumu ${asterix_args[@]} --pseudoData nominal_gen_pdf${pdPDF^^} --postfix $postfix_arg -n nominal_gen --resumUnc none --npUnc none --asUncFromUncorr --noTheoryCorrsViaHel --scalePdf 1. --pseudoDataIdx 0 --pseudoDataAxes pdfVar"
+echo
 echo "$setupRabbit_command"
-
-if ! $plotStepOnly; then 
-    setupRabbit_output=$($setupRabbit_command 2>&1 | tee /dev/tty)
-else
-    setupRabbit_output=" "
-fi
+echo
+setupRabbit_output=$($setupRabbit_command 2>&1 | tee /dev/tty)
 
 echo
-echo "rabbit_fit"
+echo "2) rabbit_fit"
 
 carrot=$(echo "$setupRabbit_output" | grep -oP '(?<=Write output file ).*')
 carrot=$(echo "$carrot" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g') # sanitize the output
+echo
 echo "Rabbit file: $carrot"
 output=$(dirname "$carrot")
 echo "Output: $output"
 
 rabbit_fit_command="rabbit_fit.py $carrot -t 0 --doImpacts --unblind --saveHists --saveHistsPerProcess --computeHistErrors -m BaseMapping -m Project ch0 absYVgen -m Project ch0 ptVgen --pseudoData nominal_gen_pdf${pdPDF^^}_pdfVar -o $output"
+echo
 echo "$rabbit_fit_command"
+echo  
+rabbit_fit_output=$($rabbit_fit_command 2>&1 | tee /dev/tty)
 
-if ! $plotStepOnly; then 
-    rabbit_fit_output=$($rabbit_fit_command 2>&1 | tee /dev/tty)
-else
-    rabbit_fit_output=" "
-fi
 
 fit_result=$(echo "$rabbit_fit_output" | grep -oP '(?<=Results written in file ).*')
-#fit_result=$(echo "$fit_result" | sed 's/\x1B\[[0-9;]*[a-zA-Z]//g') # sanitize the output
-echo "$fit_result"
 
 echo
-echo "make_postfit_pdf_grids"
+echo "3) make_postfit_pdf_grids"
 
-if ! $plotStepOnly; then python3 ${WREM_BASE}/scripts/utilities/make_postfit_pdf_grids.py --fitresult $fit_result -o $output_dir --pseudoData nominal_gen_pdf${pdPDF^^}_pdfVar -l $postfix_arg
-fi
+make_grids_cmd=(
+    python3 "${WREM_BASE}/scripts/utilities/make_postfit_pdf_grids.py"
+    --fitresult "$fit_result"
+    -o "$output_dir"
+    --pseudoData "nominal_gen_pdf${pdPDF^^}_pdfVar"
+    -l "$postfix_arg"
+)
+
+echo
+echo "${make_grids_cmd[@]}"
+echo
+make_grids_output=$("${make_grids_cmd[@]}" 2>&1 | tee /dev/tty)
+
+fitted_pdf_long_name=$(echo "$make_grids_output" | grep -oP '(?<=PDFs in set )\w+')
+pd_pdf_long_name=$(echo "$make_grids_output" | grep -oP 'pseudodata: "PDFSet<\K[^,]+')
+
+echo
+echo "4) plot pdfs"
 
 # if the output plots dir doesn't exist, create it
 output_plot_dir="${output_dir}/GenFit${fittedPDF^^}pd${pdPDF^^}/"
 if [ ! -d "$output_plot_dir" ]; then
     mkdir -p "$output_plot_dir"
 fi
+echo
 echo "Output plots directory: $output_plot_dir" 
 
+plot_cmd=(
+    python3 "${WREM_BASE}/scripts/plotting/plot_pdfs.py"
+    --lhapdf-path "$output_dir"
+    -s "$fitted_pdf_long_name" "$pd_pdf_long_name" "${fitted_pdf_long_name}_${postfix_arg}_unscaled"
+    -o "$output_plot_dir"
+    -l "${fittedPDF^^}" "${pdPDF^^}" "${fittedPDF^^} (gen fit ${pdPDF^^})"
+    -f dv uv 1 2 3
+    --colors r g b
+)
 
-compound_label_value="'${fittedPDF^^} (gen fit ${pdPDF^^})'"
-
-python3 ${WREM_BASE}/scripts/plotting/plot_pdfs.py --lhapdf-path $output_dir -s NNPDF40_nnlo_hessian_pdfas MSHT20nnlo_as118 NNPDF40_nnlo_hessian_pdfas_nnpdf40_pdMSHT20_unscaled -o $output_plot_dir -l ${fittedPDF^^} ${pdPDF^^} $compound_label_value -f dv uv 1 2 3 --colors r g b
-#echo "$plot_command"
-#plot_output=$($plot_command 2>&1 | tee /dev/tty)
+echo
+echo "${plot_cmd[@]}"
+echo
+plot_output=$("${plot_cmd[@]}" 2>&1 | tee /dev/tty)
 
 
